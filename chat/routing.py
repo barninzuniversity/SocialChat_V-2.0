57e6d666e0a64cbe9cb39a2c80037cbd402bb5dc
barninzuniversity <ibrahimzaouali888@gmail.com>
2025-03-29 23:59:13 +0100

    Delete chat directory

--- chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-# WebSocket URL patterns
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
-]
